9368ddb1b1422da6bfc1af7b4a64fc07697d3ef1
James King <hello@jamesmichaelking.com>
2021-01-14 09:41:16 +0000

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,30 @@
-// Include gulp
-var gulp = require('gulp');
+"use strict";
 
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const sass = require('gulp-sass')
 
-// Lint Task
-gulp.task('lint', function() {
-  return gulp.src('js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-var sassOptions = {
-  outputStyle: 'compressed',
-  includePaths: ['node_modules/normalize-scss/sass']
+const paths = {
+  "src": {
+    "scss": "./src/**/*.scss",
+  },
+  "dist": {
+    "css": "./dist/css/",
+    "js": "./dist/js/"
+  }
 }
 
-// Compile Our Sass
-gulp.task('sass', function() {
-  return gulp.src('src/scss/*.scss')
-    .pipe(sass(sassOptions))
-    .pipe(gulp.dest('dist/css'));
-});
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'compressed',
+  includePaths: [],
+};
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-  return gulp.src('js/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(rename('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
+gulp.task('watch-scss', () => gulp
+  .watch(paths.src.scss, gulp.series('compile-scss'))
+  .on('change',  path => console.log(`File ${path} was changed, running tasks...`))
+);
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-  gulp.watch('js/*.js', ['lint', 'scripts']);
-  gulp.watch('src/scss/*.scss', ['sass']);
-});
-
-// Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('compile-scss', () => gulp
+  .src(paths.src.scss)
+  .pipe(sass(sassOptions))
+  .pipe(gulp.dest(paths.dist.css)));
---
+++ src/README.md
@@ -0,0 +1,35 @@
+# Source
+
+## Structure
+
+
+Each folder has its own entry point (`_all.scss`).
+
+## Settings
+
+Global variables for the project, for example colour palettes and spacing and
+typography scales.
+
+This layer should not output any CSS.
+
+## Helpers
+
+Mixins that abstract common styling (for example, focus states or visually
+hidden content)
+
+This layer should not output any CSS.
+
+## Core
+
+Basic content styles for typography, links etc. The classes defined in this
+layer would generally correspond to an HTML element - for example paragraphs,
+headings, lists or links.
+
+## Components
+
+Discrete pieces of UI. In most cases these will map to the components in the
+Design System.
+
+## Utilities
+
+Utility classes – for example clearing floats, visually hiding content.
